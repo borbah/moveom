42d5a11c9d3272145658ad59fd7a82d9e3c12d33
Adam Romański <adam.romanski@netguru.pl>
2018-04-01 00:09:02 +0200

    Add dist files

---
+++ dist/precache-manifest.9dc0e50f4d6f9e83f166722eac52a2c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d59b82051f3cdeb4d655",
+    "url": "/js/vendor.d59b8205.js"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "4f4cfba55d654b9378ba",
+    "url": "/js/app.4f4cfba5.js"
+  },
+  {
+    "revision": "25b0a09d147b1d32ea4f9123cac73f8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb928ba538de019e5a84e8aa8260c4fe",
+    "url": "/img/moveom-logo.eb928ba5.png"
+  },
+  {
+    "revision": "a297d4c3084e9082bcf76d8370b880b7",
+    "url": "/img/fullscreen_01.a297d4c3.jpg"
+  },
+  {
+    "revision": "4f4cfba55d654b9378ba",
+    "url": "/css/app.45f71dd5.css"
+  }
+];
